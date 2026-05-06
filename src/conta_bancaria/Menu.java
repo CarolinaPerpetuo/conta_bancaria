6bb54e8eb37741b60bbc73a3935c2640b4862e06
Carolina Perpetuo <carolinaperpetuocosta@gmail.com>
2026-05-06 01:53:51 -0300

    C.Corrente- C.Poupança

--- src/conta_bancaria/Menu.java
+++ src/conta_bancaria/Menu.java
@@ -4,32 +4,43 @@ import java.util.Scanner;
 
 import conta_bancaria.util.Cores;
 import conta_bancaria.model.Conta;
+import conta_bancaria.model.ContaCorrente;
+import conta_bancaria.model.ContaPoupanca;
 
 public class Menu {
 
 	public static void main(String[] args) {
-		
+
 		// Teste da Classe Conta
-        Conta c1 = new Conta(1, 123, 1, "Adriana Sanches", 10000.0f);
-
-        c1.visualizar();
-
-        c1.setSaldo(15000.0f);
-	    c1.setTitular("Maria Joaquina");
+		Conta c1 = new Conta(1, 123, 1, "Adriana Sanches", 10000.0f);
+		c1.visualizar();
+		c1.sacar(12000.0f);
+		c1.visualizar();
+		c1.depositar(5000.0f);
 		c1.visualizar();
 
-        c1.sacar(12000.0f);
-		c1.visualizar();
+		// Teste da Classe Conta Corrente
+		ContaCorrente cc1 = new ContaCorrente(2, 123, 1, "José da Silva", 0.0f, 1000.0f);
+		cc1.visualizar();
+		cc1.sacar(12000.0f);
+		cc1.visualizar();
+		cc1.depositar(5000.0f);
+		cc1.visualizar();
 
-        c1.depositar(5000.0f);
-		c1.visualizar();
-		
+		// Teste da Classe Conta Poupança
+		ContaPoupanca cp1 = new ContaPoupanca(3, 123, 2, "Maria dos Santos", 100000.0f, 15);
+		cp1.visualizar();
+		cp1.sacar(1000.0f);
+		cp1.visualizar();
+		cp1.depositar(5000.0f);
+		cp1.visualizar();
+
 		Scanner leia = new Scanner(System.in);
-		
+
 		int opcao;
 
 		while (true) {
-			
+
 			System.out.println(Cores.TEXT_YELLOW + Cores.ANSI_BLACK_BACKGROUND
 					+ "*****************************************************");
 
@@ -56,52 +67,52 @@ public class Menu {
 			opcao = leia.nextInt();
 
 			if (opcao == 0) {
-				System.out.println(Cores.TEXT_WHITE_BOLD +"\nBanco do Brazil com Z - O seu Futuro começa aqui!");
+				System.out.println(Cores.TEXT_WHITE_BOLD + "\nBanco do Brazil com Z - O seu Futuro começa aqui!");
 				sobre();
-                 leia.close();
+				leia.close();
 				System.exit(0);
 			}
 
 			switch (opcao) {
-				case 1:
-					System.out.println(Cores.TEXT_WHITE_BOLD +"Criar Conta\n\n");
+			case 1:
+				System.out.println(Cores.TEXT_WHITE_BOLD + "Criar Conta\n\n");
 
-					break;
-				case 2:
-					System.out.println(Cores.TEXT_WHITE_BOLD +"Listar todas as Contas\n\n");
+				break;
+			case 2:
+				System.out.println(Cores.TEXT_WHITE_BOLD + "Listar todas as Contas\n\n");
 
-					break;
-				case 3:
-					System.out.println(Cores.TEXT_WHITE_BOLD +"Consultar dados da Conta - por número\n\n");
+				break;
+			case 3:
+				System.out.println(Cores.TEXT_WHITE_BOLD + "Consultar dados da Conta - por número\n\n");
 
-					break;
-				case 4:
-					System.out.println(Cores.TEXT_WHITE_BOLD +"Atualizar dados da Conta\n\n");
+				break;
+			case 4:
+				System.out.println(Cores.TEXT_WHITE_BOLD + "Atualizar dados da Conta\n\n");
 
-					break;
-				case 5:
-					System.out.println(Cores.TEXT_WHITE_BOLD +"Apagar a Conta\n\n");
+				break;
+			case 5:
+				System.out.println(Cores.TEXT_WHITE_BOLD + "Apagar a Conta\n\n");
 
-					break;
-				case 6:
-					System.out.println(Cores.TEXT_WHITE_BOLD +"Saque\n\n");
+				break;
+			case 6:
+				System.out.println(Cores.TEXT_WHITE_BOLD + "Saque\n\n");
 
-					break;
-				case 7:
-					System.out.println(Cores.TEXT_WHITE_BOLD +"Depósito\n\n");
+				break;
+			case 7:
+				System.out.println(Cores.TEXT_WHITE_BOLD + "Depósito\n\n");
 
-					break;
-				case 8:
-					System.out.println(Cores.TEXT_WHITE_BOLD +"Transferência entre Contas\n\n");
+				break;
+			case 8:
+				System.out.println(Cores.TEXT_WHITE_BOLD + "Transferência entre Contas\n\n");
 
-					break;
-				default:
-					System.out.println(Cores.TEXT_RED_BOLD +"\nOpção Inválida!\n");
-					break;
+				break;
+			default:
+				System.out.println(Cores.TEXT_RED_BOLD + "\nOpção Inválida!\n");
+				break;
 			}
 		}
 	}
-    
+
 	public static void sobre() {
 		System.out.println("\n*********************************************************");
 		System.out.println("Projeto Desenvolvido por: ");
@@ -109,5 +120,4 @@ public class Menu {
 		System.out.println("github.com/conteudoGeneration");
 		System.out.println("*********************************************************");
 	}
-	}
-
+}
